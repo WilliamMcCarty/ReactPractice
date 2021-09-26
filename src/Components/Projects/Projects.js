0354a7da20734b500e42684c1a7b7947b9cb3801
= <kcchief@yahoo.com>
2021-09-25 21:22:15 -0500

    Pages updated

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Banner from "../Banner";
 
-export default function Projects() {
+export default function Projects() {    
   return (
     <section className="banner text-center">
-      <Banner heading="Projects" />      
+      <Banner heading="Projects" />
+      <div className='projects'>
+        <ul>
+          <li className='projects' onClick={('')}>React Portfolio Site</li>
+          <li className='projects' onClick={('')}>ASP.NET Portfolio Site</li>
+          <li className='projects' onClick={('')}>ASP.NET Web API</li>
+          <li className='projects' onClick={('')}>ToDo React App</li>
+          <li className='projects' onClick={('')}>MVC Personal Site</li>
+          <li className='projects' onClick={('')}>ASP.NET Portfolio Site</li>
+          <li className='projects' onClick={('')}>MVC MSSQL Store Front</li>
+          <li className='projects' onClick={('')}>HTML5 CSS3 Studio Project</li>
+        </ul>
+      </div>
+      <br/>
+      <br/>
+      <br/>
     </section>
   );
 }
